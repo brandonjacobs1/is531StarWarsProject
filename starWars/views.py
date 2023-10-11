3189530c98c00762cfdaae4a1e64b1e853fd7380
brandonjacobs1 <brandon.jacobs345@gmail.com>
2023-10-11 14:36:51 -0600

    templates

--- starWars/views.py
+++ starWars/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {'data': 'test render'}
+    return render(request, 'starWars/index.html', context)
